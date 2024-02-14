142260bcafbc675a797928503c19a2d568fe2716
Arihant Jain <arihantj916@gmail.com>
2024-02-14 15:54:01 +0530

    Update code

--- src/pages/Account/SignUp.js
+++ src/pages/Account/SignUp.js
@@ -5,8 +5,7 @@ import { logoLight } from "../../assets/images";
 
 const SignUp = () => {
   const [formData, setFormData] = useState({
-    clientName: "",
-    clientLastName: "",
+    username: "",
     email: "",
     phone: "",
     password: "",
@@ -16,8 +15,7 @@ const SignUp = () => {
   });
 
   const [errors, setErrors] = useState({
-    clientName: "",
-    clientLastName: "",
+    username: "",
     email: "",
     phone: "",
     password: "",
@@ -35,14 +33,23 @@ const SignUp = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const { clientName, clientLastName, email, phone, password, confirmPassword } = formData;
+    const {
+      username,
+      email,
+      phone,
+      password,
+      confirmPassword,
+    } = formData;
     const validationErrors = {};
 
-    if (!clientName) validationErrors.clientName = "Enter your name";
-    if (!clientLastName) validationErrors.clientLastName = "Enter your last name";
+    if (!username) validationErrors.username = "Enter your username";
+    if (!clientLastName)
+      validationErrors.clientLastName = "Enter your last name";
     if (!email) validationErrors.email = "Enter your email";
-    if (password.length < 8) validationErrors.password = "Password should be of 8 letters"
-    if (password !== confirmPassword) validationErrors.confirmPassword = "Passwords do not match"
+    if (password.length < 8)
+      validationErrors.password = "Password should be of 8 letters";
+    if (password !== confirmPassword)
+      validationErrors.confirmPassword = "Passwords do not match";
     // Add more validation rules as needed...
 
     if (Object.keys(validationErrors).length > 0) {
@@ -60,31 +67,27 @@ const SignUp = () => {
         body: JSON.stringify({
           email,
           password,
+          username,
           password2: confirmPassword,
-          first_name: clientName,
-          last_name: clientLastName,
           phone_number: phone,
           role: "customer",
-          country_code: 91,
         }),
       });
       if (response.ok) {
         const data = await response.json();
         setSuccessMsg(data.detail);
-        console.log(data.detail)
+        console.log(data.detail);
       } else {
         const data = await response.json();
-        console.log(data)
+        console.log(data);
         // setSuccessMsg(data);
       }
       setFormData({
-        clientName: "",
-        clientLastName: "",
+        username: "",
         email: "",
         phone: "",
         password: "",
         confirmPassword: "",
-        countryCode: "",
         checked: false,
       });
     } catch (error) {
@@ -93,25 +96,25 @@ const SignUp = () => {
   };
 
   return (
-    <div className="w-full h-screen flex items-center justify-start">
-      <div className="w-1/2 hidden lgl:inline-flex h-full text-white">
+    <div className="flex items-center justify-start w-full h-screen">
+      <div className="hidden w-1/2 h-full text-white lgl:inline-flex">
         {/* Sidebar content */}
         <div className="w-[450px] h-full bg-primeColor px-10 flex flex-col gap-6 justify-center">
           <Link to="/">
             <img src={logoLight} alt="logoImg" className="w-28" />
           </Link>
           <div className="flex flex-col gap-1 -mt-1">
-            <h1 className="font-titleFont text-xl font-medium">
+            <h1 className="text-xl font-medium font-titleFont">
               Get started for free
             </h1>
             <p className="text-base">Create your account to access more</p>
           </div>
           <div className="w-[300px] flex items-start gap-3">
-            <span className="text-green-500 mt-1">
+            <span className="mt-1 text-green-500">
               <BsCheckCircleFill />
             </span>
             <p className="text-base text-gray-300">
-              <span className="text-white font-semibold font-titleFont">
+              <span className="font-semibold text-white font-titleFont">
                 Get started fast with Orebi
               </span>
               <br />
@@ -120,11 +123,11 @@ const SignUp = () => {
             </p>
           </div>
           <div className="w-[300px] flex items-start gap-3">
-            <span className="text-green-500 mt-1">
+            <span className="mt-1 text-green-500">
               <BsCheckCircleFill />
             </span>
             <p className="text-base text-gray-300">
-              <span className="text-white font-semibold font-titleFont">
+              <span className="font-semibold text-white font-titleFont">
                 Access all Orebi services
               </span>
               <br />
@@ -133,11 +136,11 @@ const SignUp = () => {
             </p>
           </div>
           <div className="w-[300px] flex items-start gap-3">
-            <span className="text-green-500 mt-1">
+            <span className="mt-1 text-green-500">
               <BsCheckCircleFill />
             </span>
             <p className="text-base text-gray-300">
-              <span className="text-white font-semibold font-titleFont">
+              <span className="font-semibold text-white font-titleFont">
                 Trusted by online Shoppers
               </span>
               <br />
@@ -146,16 +149,16 @@ const SignUp = () => {
             </p>
           </div>
           <div className="flex items-center justify-between mt-10">
-            <p className="text-sm font-titleFont font-semibold text-gray-300 hover:text-white cursor-pointer duration-300">
+            <p className="text-sm font-semibold text-gray-300 duration-300 cursor-pointer font-titleFont hover:text-white">
               © Orebi
             </p>
-            <p className="text-sm font-titleFont font-semibold text-gray-300 hover:text-white cursor-pointer duration-300">
+            <p className="text-sm font-semibold text-gray-300 duration-300 cursor-pointer font-titleFont hover:text-white">
               Terms
             </p>
-            <p className="text-sm font-titleFont font-semibold text-gray-300 hover:text-white cursor-pointer duration-300">
+            <p className="text-sm font-semibold text-gray-300 duration-300 cursor-pointer font-titleFont hover:text-white">
               Privacy
             </p>
-            <p className="text-sm font-titleFont font-semibold text-gray-300 hover:text-white cursor-pointer duration-300">
+            <p className="text-sm font-semibold text-gray-300 duration-300 cursor-pointer font-titleFont hover:text-white">
               Security
             </p>
           </div>
@@ -164,10 +167,13 @@ const SignUp = () => {
       <div className="w-full lgl:w-[500px] h-full flex flex-col justify-center">
         {successMsg ? (
           <div className="w-[500px]">
-            <p className="w-full px-4 py-10 text-green-500 font-medium font-titleFont">{successMsg}</p>
+            <p className="w-full px-4 py-10 font-medium text-green-500 font-titleFont">
+              {successMsg}
+            </p>
             <Link to="/signin">
-              <button className="w-full h-10 bg-primeColor rounded-md text-gray-200 text-base font-titleFont font-semibold 
-            tracking-wide hover:bg-black hover:text-white duration-300">Sign in</button>
+              <button className="w-full h-10 text-base font-semibold tracking-wide text-gray-200 duration-300 rounded-md bg-primeColor font-titleFont hover:bg-black hover:text-white">
+                Sign in
+              </button>
             </Link>
           </div>
         ) : (
@@ -179,43 +185,30 @@ const SignUp = () => {
               <div className="flex flex-col gap-3">
                 {/* Client Name */}
                 <div className="flex flex-col gap-.5">
-                  <p className="font-titleFont text-base font-semibold text-gray-600">First Name</p>
+                  <p className="text-base font-semibold text-gray-600 font-titleFont">
+                    Username
+                  </p>
                   <input
                     onChange={handleChange}
-                    value={formData.clientName}
-                    name="clientName"
+                    value={formData.username}
+                    name="username"
                     className="w-full h-8 placeholder:text-sm placeholder:tracking-wide px-4 text-base font-medium placeholder:font-normal rounded-md border-[1px] border-gray-400 outline-none"
                     type="text"
                     placeholder="eg. John Doe"
                   />
-                  {errors.clientName && (
-                    <p className="text-sm text-red-500 font-titleFont font-semibold px-4">
-                      <span className="font-bold italic mr-1">!</span>
-                      {errors.clientName}
+                  {errors.username && (
+                    <p className="px-4 text-sm font-semibold text-red-500 font-titleFont">
+                      <span className="mr-1 italic font-bold">!</span>
+                      {errors.username}
                     </p>
                   )}
                 </div>
-                {/* Client Last Name */}
-                <div className="flex flex-col gap-.5">
-                  <p className="font-titleFont text-base font-semibold text-gray-600">Last Name</p>
-                  <input
-                    onChange={handleChange}
-                    value={formData.clientLastName}
-                    name="clientLastName"
-                    className="w-full h-8 placeholder:text-sm placeholder:tracking-wide px-4 text-base font-medium placeholder:font-normal rounded-md border-[1px] border-gray-400 outline-none"
-                    type="text"
-                    placeholder="eg. John Doe"
-                  />
-                  {errors.clientLastName && (
-                    <p className="text-sm text-red-500 font-titleFont font-semibold px-4">
-                      <span className="font-bold italic mr-1">!</span>
-                      {errors.clientLastName}
-                    </p>
-                  )}
-                </div>
+
                 {/* Email */}
                 <div className="flex flex-col gap-.5">
-                  <p className="font-titleFont text-base font-semibold text-gray-600">Work Email</p>
+                  <p className="text-base font-semibold text-gray-600 font-titleFont">
+                    Work Email
+                  </p>
                   <input
                     onChange={handleChange}
                     value={formData.email}
@@ -225,15 +218,17 @@ const SignUp = () => {
                     placeholder="john@workemail.com"
                   />
                   {errors.email && (
-                    <p className="text-sm text-red-500 font-titleFont font-semibold px-4">
-                      <span className="font-bold italic mr-1">!</span>
+                    <p className="px-4 text-sm font-semibold text-red-500 font-titleFont">
+                      <span className="mr-1 italic font-bold">!</span>
                       {errors.email}
                     </p>
                   )}
                 </div>
                 {/* Phone Number */}
                 <div className="flex flex-col gap-.5">
-                  <p className="font-titleFont text-base font-semibold text-gray-600">Phone Number</p>
+                  <p className="text-base font-semibold text-gray-600 font-titleFont">
+                    Phone Number
+                  </p>
                   <input
                     onChange={handleChange}
                     value={formData.phone}
@@ -243,15 +238,17 @@ const SignUp = () => {
                     placeholder="008801234567891"
                   />
                   {errors.phone && (
-                    <p className="text-sm text-red-500 font-titleFont font-semibold px-4">
-                      <span className="font-bold italic mr-1">!</span>
+                    <p className="px-4 text-sm font-semibold text-red-500 font-titleFont">
+                      <span className="mr-1 italic font-bold">!</span>
                       {errors.phone}
                     </p>
                   )}
                 </div>
                 {/* Password */}
                 <div className="flex flex-col gap-.5">
-                  <p className="font-titleFont text-base font-semibold text-gray-600">Password</p>
+                  <p className="text-base font-semibold text-gray-600 font-titleFont">
+                    Password
+                  </p>
                   <input
                     onChange={handleChange}
                     value={formData.password}
@@ -261,15 +258,17 @@ const SignUp = () => {
                     placeholder="Create password"
                   />
                   {errors.password && (
-                    <p className="text-sm text-red-500 font-titleFont font-semibold px-4">
-                      <span className="font-bold italic mr-1">!</span>
+                    <p className="px-4 text-sm font-semibold text-red-500 font-titleFont">
+                      <span className="mr-1 italic font-bold">!</span>
                       {errors.password}
                     </p>
                   )}
                 </div>
                 {/* Confirm Password */}
                 <div className="flex flex-col gap-.5">
-                  <p className="font-titleFont text-base font-semibold text-gray-600">Confirm Password</p>
+                  <p className="text-base font-semibold text-gray-600 font-titleFont">
+                    Confirm Password
+                  </p>
                   <input
                     onChange={handleChange}
                     value={formData.confirmPassword}
@@ -279,36 +278,21 @@ const SignUp = () => {
                     placeholder="Confirm password"
                   />
                   {errors.confirmPassword && (
-                    <p className="text-sm text-red-500 font-titleFont font-semibold px-4">
-                      <span className="font-bold italic mr-1">!</span>
+                    <p className="px-4 text-sm font-semibold text-red-500 font-titleFont">
+                      <span className="mr-1 italic font-bold">!</span>
                       {errors.confirmPassword}
                     </p>
                   )}
                 </div>
-                {/* Country Code */}
-                <div className="flex flex-col gap-.5">
-                  <p className="font-titleFont text-base font-semibold text-gray-600">Country Code</p>
-                  <input
-                    onChange={handleChange}
-                    value={formData.countryCode}
-                    name="countryCode"
-                    className="w-full h-8 placeholder:text-sm placeholder:tracking-wide px-4 text-base font-medium placeholder:font-normal rounded-md border-[1px] border-gray-400 outline-none"
-                    type="text"
-                    placeholder="Your country code"
-                  />
-                  {errors.countryCode && (
-                    <p className="text-sm text-red-500 font-titleFont font-semibold px-4">
-                      <span className="font-bold italic mr-1">!</span>
-                      {errors.countryCode}
-                    </p>
-                  )}
-                </div>
+
                 {/* Checkbox */}
-                <div className="flex items-start mdl:items-center gap-2">
+                <div className="flex items-start gap-2 mdl:items-center">
                   <input
-                    onChange={(e) => setFormData({ ...formData, checked: e.target.checked })}
+                    onChange={(e) =>
+                      setFormData({ ...formData, checked: e.target.checked })
+                    }
                     checked={formData.checked}
-                    className="w-4 h-4 mt-1 mdl:mt-0 cursor-pointer"
+                    className="w-4 h-4 mt-1 cursor-pointer mdl:mt-0"
                     type="checkbox"
                   />
                   <p className="text-sm text-primeColor">
@@ -319,17 +303,20 @@ const SignUp = () => {
                 </div>
                 <button
                   onClick={handleSubmit}
-                  className={`${formData.checked
-                    ? "bg-primeColor hover:bg-black hover:text-white cursor-pointer"
-                    : "bg-gray-500 hover:bg-gray-500 hover:text-gray-200 cursor-none"
-                    } w-full text-gray-200 text-base font-medium h-10 rounded-md hover:text-white duration-300`}
+                  className={`${
+                    formData.checked
+                      ? "bg-primeColor hover:bg-black hover:text-white cursor-pointer"
+                      : "bg-gray-500 hover:bg-gray-500 hover:text-gray-200 cursor-none"
+                  } w-full text-gray-200 text-base font-medium h-10 rounded-md hover:text-white duration-300`}
                 >
                   Create Account
                 </button>
-                <p className="text-sm text-center font-titleFont font-medium">
+                <p className="text-sm font-medium text-center font-titleFont">
                   Don't have an Account?{" "}
                   <Link to="/signin">
-                    <span className="hover:text-blue-600 duration-300">Sign in</span>
+                    <span className="duration-300 hover:text-blue-600">
+                      Sign in
+                    </span>
                   </Link>
                 </p>
               </div>
